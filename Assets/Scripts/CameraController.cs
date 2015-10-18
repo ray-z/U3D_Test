33bfc0c8ee1e39231773c0dcb332524232af87a5
ray-z <710956@gmail.com>
2015-10-18 14:35:23 +0800

    add basic game for testing

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public GameObject player;
+
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () 
+	{
+		offset = this.transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		this.transform.position = player.transform.position + offset;
+	}
+}
